9e19bbdb7846eff9837cf901fb246fce5faf930e
christopherabrack@gmail.com <christopherabrack@gmail.com@eb5c8737-7115-e549-cede-1bc90705f05f>
2011-02-11 04:47:14 +0000

    updating usage and compile text

---
+++ trunk/phase2/code/src/reinstall.sh
@@ -0,0 +1,6 @@
+rm -rf *.class
+rm -rf shared_files
+rm -rf *.bin
+echo "application fully reinstalled"
+javac *.java
+echo "compile complete.  Run MyGroupServer in one terminal, MyFileServer in another, and TextUI in another as well"
