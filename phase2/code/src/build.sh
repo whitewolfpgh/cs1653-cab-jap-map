a81202e9887d694866564d3370dc4d55bc6d1791
christopherabrack@gmail.com <christopherabrack@gmail.com@eb5c8737-7115-e549-cede-1bc90705f05f>
2011-02-11 04:47:14 +0000

    updating usage and compile text

--- phase2/code/src/build.sh
+++ phase2/code/src/build.sh
@@ -1,4 +1,4 @@
 rm -rf *.class
-echo "if a fresh install is desired, remove *.bin files before executing!"
+echo "if a fresh install is desired, run ./reinstall.sh"
 javac *.java
 echo "compile complete.  Run ServerMaster in one terminal, and TextUI in another"
